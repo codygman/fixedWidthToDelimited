3bad248b613ce79dca2adb9dec1414050e46b592
Cody Goodman <codygman.consulting@gmail.com>
2017-06-29 00:18:15 -0500

    working

--- library/Example.hs
+++ library/Example.hs
@@ -6,7 +6,17 @@ module Example where
 import qualified Data.ByteString.Char8 as C8
 import qualified Data.ByteString as BS
 import Data.Monoid
-import Data.List
+-- -- import Data.List
+import Pipes
+-- import qualified Pipes.ByteString as P
+import qualified Pipes.Text as Text
+import qualified Pipes.Text.IO as Text
+import System.IO
+import Lens.Family
+import Control.Foldl
+import qualified Pipes.Group as Group
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 
 {- The test for appendAt' is as follows:
 
@@ -28,6 +38,24 @@ initialBSVal15000 = (C8.replicate (5000 * 3) 'x')
 initialOffsets15000 :: [Int]
 initialOffsets15000 = replicate 5000 3
 
+stripStart :: Monad m => Pipe T.Text T.Text m r
+stripStart = do
+    chunk <- await
+    let text = T.stripStart chunk
+    if T.null text
+      then stripStart
+      else do yield text
+              cat
+
+pFixedWidth :: Monad m => [Int] -> Pipe T.Text T.Text m r
+pFixedWidth offsets = do
+    chunk <- await
+    let text = (fixedWidthLine' offsets) chunk
+    if T.null text
+      then (pFixedWidth offsets)
+      else do yield text
+              cat
+
 {-# INLINE fixedWidthLine #-}
 {-
 it "fixedWidthLine" $ do
@@ -39,5 +67,20 @@ fixedWidthLine offsets bs = fst $ go offsets ("" :: C8.ByteString,bs)
         go [] x = x
         go (x:xs) (acc,rest) = let (newAcc,newRest) = C8.splitAt x rest in go xs (acc <> (fst $ appendAt x "|" newAcc),newRest)
 
+fixedWidthLine' :: [Int] -> T.Text -> T.Text
+fixedWidthLine' offsets txt = do
+  let bs = TE.encodeUtf8 txt
+      txt' = TE.decodeUtf8 $ fixedWidthLine offsets bs
+  txt'
 
-testLargeLine = C8.writeFile "testLargeLine.csv" (fixedWidthLine initialOffsets15000 initialBSVal15000)
+
+-- stripLines =  view Text.unlines . Group.maps (>-> Text.stripStart) . view Text.lines
+
+-- fileToFileTest = do
+--   withFile "test.fixedwidth"  ReadMode  $ \hIn  ->
+--     withFile "test.delimited" WriteMode $ \hOut ->
+--     runEffect $ Text.fromHandle hIn & Text.lines %= maps (>-> Text.stripStart) >-> Text.toHandle hOut
+
+f = withFile "test.fixedwidth"  ReadMode  $ \hIn  -> runEffect $ (view Text.unlines . Group.maps (>-> (pFixedWidth [3,4,2]) ). view Text.lines) (Text.fromHandle hIn) >-> Text.stdout
+
+takeLines n = view Text.unlines . Group.takes' n . view Text.lines
