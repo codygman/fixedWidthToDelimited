f26ea420a80393a32f77ce73bd9f13c52f4f33b4
Cody Goodman <codygman.consulting@gmail.com>
2017-06-29 00:21:31 -0500

    use text

--- library/Example.hs
+++ library/Example.hs
@@ -3,8 +3,6 @@
 -- | An example module.
 module Example where
 
-import qualified Data.ByteString.Char8 as C8
-import qualified Data.ByteString as BS
 import Data.Monoid
 -- -- import Data.List
 import Pipes
@@ -23,18 +21,18 @@ import qualified Data.Text.Encoding as TE
 it "appendAt" $ do
       appendAt 3 "|" "xxxyyyy" `shouldBe` ("xxx|yyyy","yyyy")
 -}
-appendAt :: Int -> BS.ByteString -> BS.ByteString -> (BS.ByteString,BS.ByteString)
-appendAt i appendBS bs = let (first, rest) = BS.splitAt i bs in (first <> appendBS <> rest,rest)
+appendAt :: Int -> T.Text -> T.Text -> (T.Text,T.Text)
+appendAt i appendBS bs = let (first, rest) = T.splitAt i bs in (first <> appendBS <> rest,rest)
 
 rows = 500
 width = 3
 
-initialBSVal1500 = (C8.replicate (500 * 3) 'x')
+initialBSVal1500 = (T.replicate (500 * 3) "x")
 
 initialOffsets1500  :: [Int]
 initialOffsets1500 = replicate 500 3
 
-initialBSVal15000 = (C8.replicate (5000 * 3) 'x')
+initialBSVal15000 = (T.replicate (5000 * 3) "x")
 initialOffsets15000 :: [Int]
 initialOffsets15000 = replicate 5000 3
 
@@ -47,12 +45,12 @@ stripStart = do
       else do yield text
               cat
 
-pFixedWidth :: Monad m => [Int] -> Pipe T.Text T.Text m r
-pFixedWidth offsets = do
+toDelimPipe :: Monad m => [Int] -> Pipe T.Text T.Text m r
+toDelimPipe offsets = do
     chunk <- await
-    let text = (fixedWidthLine' offsets) chunk
+    let text = (fixedWidthLine offsets) chunk
     if T.null text
-      then (pFixedWidth offsets)
+      then (toDelimPipe offsets)
       else do yield text
               cat
 
@@ -61,18 +59,11 @@ pFixedWidth offsets = do
 it "fixedWidthLine" $ do
       fixedWidthLine [2,3,4] "xxyyyzzzz" `shouldBe` "xx|yyy|zzzz|"
 -}
-fixedWidthLine :: [Int] -> C8.ByteString -> C8.ByteString
-fixedWidthLine offsets bs = fst $ go offsets ("" :: C8.ByteString,bs)
-  where go :: [Int] -> (C8.ByteString,C8.ByteString) -> (C8.ByteString,C8.ByteString)
+fixedWidthLine :: [Int] -> T.Text -> T.Text
+fixedWidthLine offsets bs = fst $ go offsets ("" :: T.Text,bs)
+  where go :: [Int] -> (T.Text,T.Text) -> (T.Text,T.Text)
         go [] x = x
-        go (x:xs) (acc,rest) = let (newAcc,newRest) = C8.splitAt x rest in go xs (acc <> (fst $ appendAt x "|" newAcc),newRest)
-
-fixedWidthLine' :: [Int] -> T.Text -> T.Text
-fixedWidthLine' offsets txt = do
-  let bs = TE.encodeUtf8 txt
-      txt' = TE.decodeUtf8 $ fixedWidthLine offsets bs
-  txt'
-
+        go (x:xs) (acc,rest) = let (newAcc,newRest) = T.splitAt x rest in go xs (acc <> (fst $ appendAt x "|" newAcc),newRest)
 
 -- stripLines =  view Text.unlines . Group.maps (>-> Text.stripStart) . view Text.lines
 
@@ -81,6 +72,6 @@ fixedWidthLine' offsets txt = do
 --     withFile "test.delimited" WriteMode $ \hOut ->
 --     runEffect $ Text.fromHandle hIn & Text.lines %= maps (>-> Text.stripStart) >-> Text.toHandle hOut
 
-f = withFile "test.fixedwidth"  ReadMode  $ \hIn  -> runEffect $ (view Text.unlines . Group.maps (>-> (pFixedWidth [3,4,2]) ). view Text.lines) (Text.fromHandle hIn) >-> Text.stdout
+testDelimToFixed = withFile "test.fixedwidth"  ReadMode  $ \hIn  -> runEffect $ (view Text.unlines . Group.maps (>-> (toDelimPipe [3,4,2]) ). view Text.lines) (Text.fromHandle hIn) >-> Text.stdout
 
 takeLines n = view Text.unlines . Group.takes' n . view Text.lines
